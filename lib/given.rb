79a49aa06b9bc18ae9aa09aba958e2d1b0e17727
Maher Hawash <gmhawash@gmail.com>
2012-08-23 08:33:22 -0700

    require files before constantize class

--- lib/given.rb
+++ lib/given.rb
@@ -4,6 +4,8 @@ class TestHarness
     include TestHarness::TestHelper
 
     Dir.glob(Rails.root.join('app', 'test_harness', 'given/*.rb')).each do |file|
+      component =  File.basename(file, '.rb')
+      require "given/#{component}"
       klass = ("TestHarness::Given::%s" % File.basename(file, '.rb').camelize).constantize
       include klass
     end
